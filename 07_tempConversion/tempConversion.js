b5d8106289e52d6df4a82e980c4a6ed52c559196
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2023-07-19 13:07:17 +0200

    resolve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let result = 0;
+  result = (degree-32)*5/9;
+  return Math.round(result * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let result = 0;
+  result = (degree * 9/5) + 32;
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
