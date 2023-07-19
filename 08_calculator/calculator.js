d0605c73de9eb0760f7a95dc9662b929aa16b598
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2023-07-19 13:08:07 +0200

    resolve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,50 @@
-const add = function() {
+const add = function(a, b) {
+  return a+b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  return a-b;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  let reduce = arr.reduce((total, item)=>{
+    return total + item;
+
+  },0);
+  return reduce;
 	
 };
 
-const multiply = function() {
+const multiply = function(...arr) {
+  let reduce = arr.reduce((total, item) => {
+    return total * item;
+
+  },1)
+
+  return reduce;
 
 };
 
-const power = function() {
+const power = function(nbr, pwr) {
+  let result=1;
+  for(let i = 1; i <= pwr; i++ ){
+    result *= nbr;
+
+  }
+  return result;
 	
 };
 
-const factorial = function() {
+const factorial = function(nbr) {
+  let result = 1;
+  if (nbr === 0) return 1;
+  for(let i = nbr; i >= 1; i--){
+    result *=i ;
+  }
+  return result;
+
 	
 };
 
