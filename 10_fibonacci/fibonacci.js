29554879ecfbdec375d49128d8db90c0698bebed
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2023-07-19 17:42:20 +0200

    resolve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let myArr=[];
+    if(n<0){
+        return "OOPS";
+    }
+    let nbr = parseInt(n);
+    
+    for(let i = 0; i < nbr; i++){
+        if(i == 0 || i == 1){
+            x = 1;
+            myArr.push(x);
+        }
+        else{
+            x = myArr[i-1] + myArr[i-2];
+            myArr.push(x);
+        }
+        }
+        return myArr[myArr.length-1];
+    
+    
 };
 
 // Do not edit below this line
