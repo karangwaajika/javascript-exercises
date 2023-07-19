67f2c95d0293a1be115a0a30217ac87830798143
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2023-07-19 13:04:33 +0200

    resolve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(nbr1, nbr2) {
+    let sumOfInterval = 0;
+    let maxNumber=0;
+    let minNumber=0;
+    if(nbr2 >= nbr1){
+        maxNumber = nbr2;
+        minNumber = nbr1;
+    }
+    else{
+        maxNumber = nbr1;
+        minNumber = nbr2;
+    }
+    for(let i = minNumber; i <= maxNumber; i++){
+        sumOfInterval += i; 
+    }
+    return sumOfInterval;
 
 };
 
