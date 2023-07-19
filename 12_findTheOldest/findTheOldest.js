e0c0b6e069ea8059b45d07f0455240cc1cadb780
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2023-07-19 17:46:26 +0200

    12_findTheOldest part 1

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const oldest = arr.reduce((x,y)=>{
+        let one =  x.yearOfDeath - x.yearOfBirth;
+        let nextOne =  y.yearOfDeath - y.yearOfBirth;
+        if(one > nextOne){
+            return x.name;
+        }
+        else{
+            return y
+        }
+    });
+    return oldest;
+
 
 };
-
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+console.log(findTheOldest(people))
 // Do not edit below this line
 module.exports = findTheOldest;
